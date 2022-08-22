92e98f1ff7e1cc81f224e105dcbbb84ccd8cf062
Çetin Yılmaz <108508942+cetinylmz@users.noreply.github.com>
2022-08-22 12:25:43 +0300

    sa

--- user/graphql/resolvers/index.js
+++
@@ -1,7 +0,0 @@
-const path = require('path')
-const { mergeResolvers } = require('@graphql-tools/merge')
-const { loadFilesSync } = require('@graphql-tools/load-files')
-
-const resolversArray = loadFilesSync(path.join(__dirname), { extensions: ['js'] })
-
-module.exports = mergeResolvers(resolversArray)
--- user/graphql/typeDefs/index.js
+++
@@ -1,7 +0,0 @@
-const path = require('path')
-const { loadFilesSync } = require('@graphql-tools/load-files')
-const { mergeTypeDefs } = require('@graphql-tools/merge')
-
-const typesArray = loadFilesSync(path.join(__dirname), { extensions: ['graphql'] })
-
-module.exports = mergeTypeDefs(typesArray)
